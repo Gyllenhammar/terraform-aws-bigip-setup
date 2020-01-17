a99025edbc4494da66f9964eda5a8165a72cfc73
Gyllenhammar <Gyllenhammar@users.noreply.github.com>
2020-01-17 11:01:43 +0100

    Changed Varaibles - Added regions

--- variables.tf
+++ variables.tf
@@ -5,40 +5,31 @@ variable "SecretAccessKey" {}
 variable "prefix" {
   default = "tf-aws-bigip"
 }
-## Europe Regions need oder Jumphost and BigIP Instance Typs1
+## Different regions need other Jumphost and BigIP Instance Types
 ## Uncomment needed region below
 
-# US (Oregon)
 variable "region" {
-  default = "us-west-2"
+  # default = "eu-central-1"  # Europe (Frankfurt)
+  # default = "eu-north-1"    # Europe (Stockholm)
+  # default = "us-west-2"     # US (Oregon)
 }
 
 variable "azs" {
-  default = ["us-west-2a", "us-west-2b"]
+  # default = ["eu-central-1a", "eu-central-1b"]  # Europe (Frankfurt)
+  # default = ["eu-north-1a", "eu-north-1b"]      # Europe (Stockholm)
+  # default = ["us-west-2a", "us-west-2b"]        # US (Oregon)
 }
 
 variable "ec2_bigip_type" {
-  default = "c4.xlarge"
+  # default = "c4.xlarge"   # Europe (Frankfurt)
+  # default = "c5.xlarge"   # Europe (Stockholm)
+  # default = "c4.xlarge"   # US (Oregon)
 }
 variable "ec2_ubuntu_type" {
-  default = "t2.xlarge"
+  # default = "t2.xlarge"   #Europe (Frankfurt)
+  # default = "t3.xlarge"   # Europe (Stockholm)
+  # default = "t2.xlarge"   # US (Oregon)
 }
-
-## Europe (Stockholm)
-# variable "region" {
-#   default = "eu-north-1"
-# }
-#
-# variable "azs" {
-#   default = ["eu-north-1a", "eu-north-1b"]
-# }
-#
-# variable "ec2_bigip_type" {
-#   default = "c5.xlarge"
-# }
-# variable "ec2_ubuntu_type" {
-#   default = "t3.xlarge"
-# }
 
 variable "cidr" {
   default = "10.0.0.0/16"
